71bb53629b359206ec97f720c19c0704ea2836be
Samuel Molnar <molnar.samuel@gmail.com>
2020-06-21 13:05:22 +0200

    Fix credentials

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ Rails.application.routes.draw do
     Sidekiq::Web.use Rack::Auth::Basic do |username, password|
       ActiveSupport::SecurityUtils.secure_compare(
         ::Digest::SHA256.hexdigest(username),
-        ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])
+        ::Digest::SHA256.hexdigest(Rails.application.credentials.dig(:sidekiq, :username))
       ) &&
         ActiveSupport::SecurityUtils.secure_compare(
           ::Digest::SHA256.hexdigest(password),
-          ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD'])
+          ::Digest::SHA256.hexdigest(Rails.application.credentials.dig(:sidekiq, :password))
         )
     end
   end
