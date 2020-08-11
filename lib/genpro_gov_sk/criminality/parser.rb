300f969d5fc19f577f6b073e1300784cb9906878
Samuel Molnar <molnar.samuel@gmail.com>
2020-08-11 18:06:17 +0200

    WIP Add stats for criminality

--- lib/genpro_gov_sk/criminality/parser.rb
+++ lib/genpro_gov_sk/criminality/parser.rb
@@ -11,40 +11,53 @@ module GenproGovSk
             hash[name] = csv[0].index(key)
           end
 
-        offices.map do |name, column|
-          statistics = [PeopleFilter.process(csv, column: column), ClosureFilter.process(csv, column: column)].compact
+        map = {
+          'V trestných registroch Pv/Kv/Gv napadlo spisov' => :incoming_cases,
+          'V trestných registroch Pv/Kv/Gv bolo vybavených spisov' => :closed_cases,
+          'Počet známych odstíhaných osôb (na prokuratúre skončené)' => :known_closed_prosecuted_people,
+          'Počet obžalovaných osôb' => :prosecuted_people,
+          'Obžalovaní recidivisti' => :prosecuted_recidivists,
+          'Skladba odstíhaných osôb muži' => :men,
+          'Skladba odstíhaných osôb ženy' => :women,
+          'Skladba odstíhaných osôb mladiství' => :young,
+          'Skladba odstíhaných osôb cudzinci' => :foreigners,
+          'Skladba odstíhaných osôb vplyv alkoholu' => :alcohol_abuse,
+          'Skladba odstíhaných osôb vplyv inej návykovej látky' => :substance_abuse,
+          'Dohoda o vine a treste odoslaná na súd' => :guilt_and_punishment_aggreement
+        }
 
-          { office: name, statistics: statistics }
+        offices.map do |name, index|
+          last_title = nil
+
+          statistics =
+            csv.map do |row|
+              last_title = row[0] if row[0]&.strip.presence
+
+              filter =
+                map[Normalizer.normalize_filter(row[0]&.strip.presence ? row[0] : [last_title, row[1]].join(' '))]
+
+              next unless filter
+
+              count = Normalizer.parse_count(row[index])
+
+              next unless count
+
+              { filters: [filter], count: count }
+            end
+
+          { name: name, year: year, statistics: statistics.compact }
         end
       end
 
-      class PeopleFilter
-        def self.process(csv, column:)
-          row = csv.find { |e| e[0] == 'Skladba odstíhaných osôb' }
-          values = ParseSublist.parse(csv, row: row, column: column)
-
-          { type: :people, values: values }
+      class Normalizer
+        def self.normalize_filter(value)
+          value.to_s.gsub(/[[:space:]]+/, ' ').strip
         end
-      end
 
-      class ClosureFilter
-        def self.process(csv, column:); end
-      end
+        def self.parse_count(value)
+          count = value.to_s.gsub(/[[:space:]]/, '')
 
-      class ParseSublist
-        def self.parse(csv, row:, column:)
-          i = csv.index(row) + 1
-          result = {}
-
-          while !csv[i][0].presence
-            value = csv[i][column]
-
-            result[csv[i][1].strip] = Integer(value.gsub(/[[:space:]]/, '')) if value
-
-            i += 1
-          end
-
-          result
+          count.presence ? Integer(count) : nil
         end
       end
     end
