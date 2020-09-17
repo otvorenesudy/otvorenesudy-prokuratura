e4be65216bdca203fda80272180f8a88037249fe
Samuel Molnar <molnar.samuel@gmail.com>
2020-09-18 00:02:26 +0200

    Open collapse if hidden values are selected

--- app/helpers/facets_helper.rb
+++ app/helpers/facets_helper.rb
@@ -1,11 +1,12 @@
 module FacetsHelper
-  def collapsible_facet_results(facets, limit: 5, &renderer)
+  def collapsible_facet_results(facets, selected: [], limit: 5, &renderer)
     visible, hidden = facets.to_a[0...limit], facets.to_a[limit..-1]
     visible, hidden = visible.to_h, hidden.to_h if facets.is_a?(Hash)
     id = "collapsible-facet-values-#{SecureRandom.hex}"
 
     render(
-      partial: 'facets/collapsible_results', locals: { id: id, visible: visible, hidden: hidden, renderer: renderer }
+      partial: 'facets/collapsible_results',
+      locals: { id: id, visible: visible, hidden: hidden, renderer: renderer, selected: selected }
     )
   end
 end
