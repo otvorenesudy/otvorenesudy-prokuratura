a5304b3e811e052f30dc423cc6de272444688207
Samuel Molnar <molnar.samuel@gmail.com>
2020-11-08 21:34:44 +0100

    Use better titles

--- app/services/google_news.rb
+++ app/services/google_news.rb
@@ -21,8 +21,10 @@ class GoogleNews
       source = 'SME' if uri.host.match('sme.sk')
       source = 'Denník N' if uri.host.match('dennikn.sk')
       source = 'Aktuality' if uri.host.match('aktuality.sk')
+      title = result.pagemap['metatags']&.find { |e| e['og:title'] }&.fetch('og:title', nil) || result.title
+      snippet = result.pagemap['metatags']&.find { |e| e['og:title'] }&.fetch('og:description', nil) || result.snippet
 
-      { title: result.title, snippet: result.snippet.gsub(/[[:space:]]+/, ' '), url: result.link, source: source }
+      { title: title, snippet: snippet.gsub(/[[:space:]]+/, ' '), url: result.link, source: source }
     end
   end
 
