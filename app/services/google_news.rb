fa091fb4b41b7e920976478076d154cb369ab966
Samuel Molnar <molnar.samuel@gmail.com>
2020-11-28 10:08:03 +0100

    Fix search results

--- app/services/google_news.rb
+++ app/services/google_news.rb
@@ -4,40 +4,42 @@ class GoogleNews
   def self.search_by(name)
     service = Google::Apis::CustomsearchV1::CustomSearchAPIService.new
     service.key = Rails.application.credentials.dig(:google, :api, :search, :key)
+    pages = [0, 11]
 
-    results =
-      service.list_cses(
-        exact_terms: name,
-        or_terms: 'prokurátor|prokuratúra',
-        cx: Rails.application.credentials.dig(:google, :api, :search, :id),
-        sort: 'date',
-        num: 10,
-        safe: 'active'
-      ).items
+    pages.map do |start|
+      results =
+        service.list_cse_siterestricts(
+          exact_terms: name,
+          or_terms: 'prokurátor|prokuratúra',
+          cx: Rails.application.credentials.dig(:google, :api, :search, :id),
+          num: 10,
+          start: start
+        ).items
 
-    searched_at = Time.zone.now
+      searched_at = Time.zone.now
 
-    return [] if results.blank?
+      return [] if results.blank?
 
-    results.map do |result|
-      uri = URI.parse(result.link)
-      source = 'SME' if uri.host.match('sme.sk')
-      source = 'Denník N' if uri.host.match('dennikn.sk')
-      source = 'Aktuality' if uri.host.match('aktuality.sk')
-      title =
-        result.pagemap.try { |e| e['metatags']&.find { |e| e['og:title'] }&.fetch('og:title', nil) } || result.title
-      snippet =
-        result.pagemap.try { |e| e['metatags']&.find { |e| e['og:title'] }&.fetch('og:description', nil) } ||
-          result.snippet
+      results.map do |result|
+        uri = URI.parse(result.link)
+        source = 'SME' if uri.host.match('sme.sk')
+        source = 'Denník N' if uri.host.match('dennikn.sk')
+        source = 'Aktuality' if uri.host.match('aktuality.sk')
+        title =
+          result.pagemap.try { |e| e['metatags']&.find { |e| e['og:title'] }&.fetch('og:title', nil) } || result.title
+        snippet =
+          result.pagemap.try { |e| e['metatags']&.find { |e| e['og:title'] }&.fetch('og:description', nil) } ||
+            result.snippet
 
-      {
-        title: title,
-        snippet: snippet.gsub(/[[:space:]]+/, ' '),
-        url: result.link,
-        source: source,
-        searched_at: searched_at.to_i
-      }
-    end
+        {
+          title: title,
+          snippet: snippet.gsub(/[[:space:]]+/, ' '),
+          url: result.link,
+          source: source,
+          searched_at: searched_at.to_i
+        }
+      end
+    end.flatten
   end
 
   def self.search_url_for(record)
@@ -46,13 +48,8 @@ class GoogleNews
     } prokurátor prokuratúra site:dennikn.sk OR site:aktuality.sk OR site:sme.sk"
   end
 
-  def self.cache_for(model, size: 50, ids: nil)
-    unless ids
-      ids = model.where(news: nil).first(size)
-      ids += model.where.not(news: nil).order("(news -> 'last_updated_at') :: int").limit(size - ids.size).pluck(:id)
-    end
-
-    model.where(id: ids).find_each do |record|
+  def self.cache_for(model)
+    model.find_each do |record|
       results = GoogleNews.search_by(record.to_news_query)
       urls = results.map { |e| e[:url] }
       data = record.news ? record.news['data'] : []
