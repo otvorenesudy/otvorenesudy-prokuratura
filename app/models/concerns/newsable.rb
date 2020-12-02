1af85bf26649b3a6ea0c44bd8c25931c646405cb
Samuel Molnar <molnar.samuel@gmail.com>
2020-12-02 23:45:10 +0100

    Implement news anonymization

--- app/models/concerns/newsable.rb
+++ app/models/concerns/newsable.rb
@@ -1,12 +1,21 @@
 module Newsable
   extend ActiveSupport::Concern
 
-  EXCLUDED_URLS = [
-    'https://e.dennikn.sk/682996/sef-agrokomplexu-z-sns-uz-stihol-zamestnat-dceru-poslankyne-a-pohnevat-si-50-firiem/1738949',
-    ''
+  EXCLUDED_URLS = %w[
+    https://e.dennikn.sk/682996/sef-agrokomplexu-z-sns-uz-stihol-zamestnat-dceru-poslankyne-a-pohnevat-si-50-firiem/1738949
   ]
 
-  class_methods {}
+  class_methods do
+    def remove_excluded_urls_from_news
+      find_each do |record|
+        next if !record.news || !record.news['data']
+
+        record.news['data'] = record.news['data'].reject { |e| EXCLUDED_URLS.any? { |url| e['url'].match(url) } }
+
+        record.save!
+      end
+    end
+  end
 
   def articles
     news ? news['data'].first(20) : []
