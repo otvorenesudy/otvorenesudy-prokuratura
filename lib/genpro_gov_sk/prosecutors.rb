2798c32e79a12bc52e62cae5714c6ea9165dad2e
Samuel Molnar <molnar.samuel@gmail.com>
2021-01-06 11:30:29 +0100

    Fix import again

--- lib/genpro_gov_sk/prosecutors.rb
+++ lib/genpro_gov_sk/prosecutors.rb
@@ -1,13 +1,16 @@
 require 'p_d_f_extractor'
+require 'legacy'
 
 module GenproGovSk
   module Prosecutors
+    using ::Legacy::String
+
     def self.import(force: false)
       html =
         Curl.get('https://www.genpro.gov.sk/prokuratura-sr/menny-zoznam-prokuratorov-slovenskej-republiky-3928.html')
           .body_str
 
-      path = Nokogiri.HTML(html).css('a').find { |e| e.text =~ /Menný zoznam prokurátorov SR/ }[:href]
+      path = Nokogiri.HTML(html).css('a').find { |e| e.text.ascii =~ /Menny zoznam prokuratorov SR/ }[:href]
       url = "https://www.genpro.gov.sk#{path}"
 
       text, file = PDFExtractor.extract_text_from_url(url)
