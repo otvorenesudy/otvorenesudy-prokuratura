5bcca79953c7d85d92af63e785a5c00b053c9fc6
Samuel Molnar <molnar.samuel@gmail.com>
2024-05-08 21:04:28 +0200

    Add Rack::Attack

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,8 +18,7 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :dalli_store
-    config.public_file_server.headers = { 'Cache-Control' => "public, max-age=#{2.days.to_i}" }
+    config.cache_store = :file_store, Rails.root.join('tmp', 'cache')
   else
     config.action_controller.perform_caching = false
 
---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,19 @@
+if Rails.env.production?
+  Rack::Attack.blocklist('Pentesters') do |req|
+    Rack::Attack::Fail2Ban.filter("pentesters-#{req.ip}", maxretry: 3, findtime: 10.minutes, bantime: 5.minutes) do
+      CGI.unescape(req.query_string) =~ %r{/etc/passwd} || req.path.include?('/etc/passwd') ||
+        req.path.include?('wp-admin') || req.path.include?('wp-login')
+    end
+  end
+
+  Rack::Attack.throttle('req/ip', limit: 10, period: 5.seconds) { |req| req.ip unless req.path.match(%r{\A/sidekiq}) }
+
+  Rack::Attack.blocklist('Block IP') do |req|
+    Rack::Attack::Allow2Ban.filter("throttle-ban-#{req.ip}", maxretry: 10, findtime: 5.seconds, bantime: 1.hours) do
+      !req.path.match(%r{\A/sidekiq})
+    end
+  end
+
+  Rack::Attack.blocklisted_responder = lambda { |request| [503, {}, ['Blocked']] }
+  Rack::Attack.throttled_responder = lambda { |request| [503, {}, ["Server Error\n"]] }
+end
